551ec26409dd80498f00a783c7249aa509b1bae2
ArielGoldzweig <agoldzweig@uc.cl>
2023-12-07 23:15:57 -0300

    feat: try test get

--- tests/controllers/meetings/client/meetings.controller.test.ts
+++ tests/controllers/meetings/client/meetings.controller.test.ts
@@ -1,11 +1,24 @@
 import request from 'supertest';
 import app from '../../../../src/app';
 
+// describe('GET /meetings/client', () => {
+//     it('should return meetings for a specific client', async () => {
+//       const response = await request(app).get('/meetings/client').send({ clientId: 'client-id' });
+//       expect(response.statusCode).toBe(200);
+//       expect(Array.isArray(response.body)).toBe(true);
+//     });
+//   });
+
 describe('GET /meetings/client', () => {
-    it('should return meetings for a specific client', async () => {
-      const response = await request(app).get('/meetings/client').send({ clientId: 'client-id' });
-      expect(response.statusCode).toBe(200);
-      expect(Array.isArray(response.body)).toBe(true);
-    });
+  it('should return meetings for a specific client', async () => {
+    const userId = 'your-user-id';  // Reemplaza 'your-user-id' con el ID real del usuario
+
+    const response = await request(app)
+      .get('/meetings/client')
+      .set('userId', userId);
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
   });
+});
   
--- tests/controllers/meetings/user/meetings.controller.test.ts
+++ tests/controllers/meetings/user/meetings.controller.test.ts
@@ -1,6 +1,12 @@
 import request from 'supertest';
 import app from '../../../../src/app';
 
+// beforeEach(() => async () => {
+//   await request(app)
+//       .post('/auth/login')
+//       .send({ email: 'test2@example.com', id: '123', firstName: 'Test User', role: 'EMPLEADO' });
+// });
+
 describe('POST /meetings/user', () => {
     it('should create a new meeting and return 200 status code', async () => {
       const response = await request(app)
