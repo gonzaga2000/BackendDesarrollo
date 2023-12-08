55e333735a4cc1a2b70e1c3dc9d9ee7ffc7814c9
ArielGoldzweig <agoldzweig@uc.cl>
2023-12-07 22:53:31 -0300

    add: jest mock

--- tests/controllers/auth/user/auth.controller.test.ts
+++ tests/controllers/auth/user/auth.controller.test.ts
@@ -1,11 +1,22 @@
 import request from 'supertest';
 import app from '../../../../src/app';
+import { MockContext, Context, createMockContext } from '../../../controllers/context';
+
+
+let mockCtx: MockContext
+let ctx: Context
+
+beforeEach(() => {
+  mockCtx = createMockContext()
+  ctx = mockCtx as unknown as Context
+})
+
 
 describe('POST /auth/login', () => {
   it('should create a new user and return 200 status code', async () => {
     const response = await request(app)
       .post('/auth/login')
-      .send({ email: 'test@example.com', id: '123', name: 'Test User', role: 'CLIENT' });
+      .send({ email: 'test@example.com', id: '123', firstName: 'Test User', role: 'CLIENT' });
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty('id');
   });
