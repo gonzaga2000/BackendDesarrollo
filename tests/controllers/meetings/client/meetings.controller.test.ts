c47ce769794620a4ce908d3147425de73eb66daf
Benjamin Guiloff <byguiloff@uc.cl>
2023-12-10 23:43:10 -0300

    add: more tests

--- tests/controllers/meetings/client/meetings.controller.test.ts
+++ tests/controllers/meetings/client/meetings.controller.test.ts
@@ -7,5 +7,18 @@ describe('GET /meetings/client', () => {
       expect(response.statusCode).toBe(200);
       expect(Array.isArray(response.body)).toBe(true);
     });
+
+    it('should handle non-existent clients', async () => {
+      const response = await request(app).get('/meetings/client').send({ clientId: 'non-existent-client-id' });
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toHaveProperty('message', 'No se encontró el cliente');
+    });
+
+    it('should handle missing client ID', async () => {
+      const response = await request(app).get('/meetings/client');
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty('message', 'El ID del cliente es requerido');
+    });
+    
   });
   
--- tests/controllers/meetings/user/meetings.controller.test.ts
+++ tests/controllers/meetings/user/meetings.controller.test.ts
@@ -1,7 +1,8 @@
 import request from 'supertest';
 import app from '../../../../src/app';
 
-describe('POST /meetings/user', () => {
+describe('User Meetings Controller', () => {  
+  describe('POST /meetings/user', () => {
     it('should create a new meeting and return 200 status code', async () => {
       const response = await request(app)
         .post('/meetings/user')
@@ -10,3 +11,37 @@ describe('POST /meetings/user', () => {
       expect(response.body).toHaveProperty('id');
     });
   });
+
+  describe('GET /meetings/user', () => {
+    it('should return 200 status code', async () => {
+      const response = await request(app).get('/meetings/user');
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  describe('GET /meetings/user/:id', () => {
+    it('should return 200 status code', async () => {
+      const response = await request(app).get('/meetings/user/1');
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  describe('PUT /meetings/user/:id', () => {
+    it('should update a meeting and return 200 status code', async () => {
+      const meetingId = 1; // Asegúrate de que este ID exista en tu base de datos de pruebas
+      const updatedData = {
+        description: 'Updated Meeting Description',
+        fechaReunion: '2023-12-10'
+      };
+
+      const response = await request(app)
+        .put(`/meetings/user/${meetingId}`)
+        .send(updatedData);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toHaveProperty('message', 'meeting actualizada correctamente');
+    });
+  });
+
+
+});
